58b48a67af36581201128f291894af1aced21a9e
Elaine CBR <mail_to_elaine@yahoo.de>
2025-12-12 00:52:45 -0300

    Update user.sql with new user entries

--- user.sql
+++ user.sql
@@ -1,7 +1,8 @@
 BEGIN;
 
-INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(1, 'Carlos Santos', 'carlossantos@gmail.com', '21999567788', '2025-11-27 12:34:51.725380');
+INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(1, 'Elaine Chagas', 'elainechagas@gmail.com', '21999327755', '2025-12-06 05:48:15.059477');
 INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(2, 'Usuario-test', 'usertest@example.com', '10999438867', '2025-11-27 13:35:35.389056');
+INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(3, 'Carlos Santos', 'carlossantos@gmail.com', '21999567788', '2025-11-27 12:34:51.725380');
 INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(4, 'Elaine Teste', 'elaine.teste@example.com', '11988887777', '2025-11-27 14:27:54.923052');
 INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(7, 'PucUsuario Teste', 'PucUsuario@test.com.br', '21999337899', '2025-11-29 23:36:34.690745');
 INSERT INTO user(id, nome_completo, email, telefone, data_cadastro) VALUES(8, 'Mário Cardoso', 'mariocardoso@gmail.com', '21999202044', '2025-11-29 23:44:04.462719');
